ad531511604c23e90bf6e8eeed6c1e82a45b106c
Bugnutiy <bigbag.minecrafter@gmail.com>
2024-09-07 12:39:58 +0300

    Timer Fix

--- lib/Timer/Timer.h
+++ lib/Timer/Timer.h
@@ -8,37 +8,16 @@
  *
  * @return The macro does not return a value.
  */
-#define TMR8_S(T, CODE) \
-  ({static uint8_t tmr##T = millis();              \
-    if ((uint8_t)(millis() - tmr##T) >= T)  \
-    {                                       \
-      tmr##T +=T;                           \
-      CODE                                  \
-    } })
-#define TMR32(T, CODE) \
-  ({static uint32_t tmr##T = 0;             \
-    if ((uint32_t)(millis() - tmr##T) >= T) \
-    {                                       \
-      tmr##T = millis();                    \
-      CODE                                  \
-    } })
+#define TMR64(T, CODE)            \
+  {                               \
+    static uint64_t tmr##T = 0;   \
+    if ((millis() - tmr##T) >= T) \
+    {                             \
+      tmr##T = millis();          \
+      CODE                        \
+    }                             \
+  }
 
-
-#define TMR16(T, CODE) \
-  ({static uint16_t tmr##T = 0;                 \
-    if ((uint16_t)(millis() - tmr##T) >= T) \
-    {                                       \
-      tmr##T = millis();                    \
-      CODE                                     \
-    } })
-
-#define TMR8(T, CODE) \
-  ({static uint8_t tmr##T = 0;                 \
-    if ((uint8_t)(millis() - tmr##T) >= T) \
-    {                                       \
-      tmr##T = millis();                    \
-      CODE                                     \
-    } })
 /**
  * @brief Defines a macro for a timer that triggers after a specified time interval, with a specified code block to execute.
  * The timer uses the millis() function to measure the elapsed time.
@@ -48,20 +27,16 @@
  *
  * @return The macro does not return a value.
  */
-#define TMR8_S(T, CODE) \
-  ({static uint8_t tmr##T = millis();              \
-    if ((uint8_t)(millis() - tmr##T) >= T)  \
-    {                                       \
-      tmr##T +=T;                           \
-      CODE                                  \
-    } })
-#define TMR32_S(T, CODE) \
-  ({static uint32_t tmr##T = millis();                 \
-    if ((uint32_t)(millis() - tmr##T) >= T) \
-    {                                       \
-      tmr##T += T;                    \
-      CODE                                     \
-    } })
+#define TMR32(T, CODE)                        \
+  {                                           \
+    static uint32_t tmr##T = 0;               \
+    if ((((uint32_t)millis()) - tmr##T) >= T) \
+    {                                         \
+      tmr##T = millis();                      \
+      CODE                                    \
+    }                                         \
+  }
+
 /**
  * @brief Defines a macro for a timer that triggers after a specified time interval, with a specified code block to execute.
  * The timer uses the millis() function to measure the elapsed time.
@@ -71,20 +46,16 @@
  *
  * @return The macro does not return a value.
  */
-#define TMR8_S(T, CODE) \
-  ({static uint8_t tmr##T = millis();              \
-    if ((uint8_t)(millis() - tmr##T) >= T)  \
-    {                                       \
-      tmr##T +=T;                           \
-      CODE                                  \
-    } })
-#define TMR16_S(T, CODE) \
-  ({static uint16_t tmr##T = millis();                 \
-    if ((uint16_t)(millis() - tmr##T) >= T) \
-    {                                       \
-      tmr##T +=T;                    \
-      CODE                                     \
-    } })
+#define TMR16(T, CODE)                        \
+  {                                           \
+    static uint16_t tmr##T = 0;               \
+    if ((((uint16_t)millis()) - tmr##T) >= T) \
+    {                                         \
+      tmr##T = millis();                      \
+      CODE                                    \
+    }                                         \
+  }
+
 /**
  * @brief Defines a macro for a timer that triggers after a specified time interval, with a specified code block to execute.
  * The timer uses the millis() function to measure the elapsed time.
@@ -94,10 +65,88 @@
  *
  * @return The macro does not return a value.
  */
-#define TMR8_S(T, CODE) \
-  ({static uint8_t tmr##T = millis();              \
-    if ((uint8_t)(millis() - tmr##T) >= T)  \
-    {                                       \
-      tmr##T +=T;                           \
-      CODE                                  \
-    } })
+#define TMR8(T, CODE)                        \
+  {                                          \
+    static uint8_t tmr##T = 0;               \
+    if ((((uint8_t)millis()) - tmr##T) >= T) \
+    {                                        \
+      tmr##T = millis();                     \
+      CODE                                   \
+    }                                        \
+  }
+
+/**
+ * @brief Defines a macro for a timer that triggers after a specified time interval, with a specified code block to execute.
+ * The timer uses the millis() function to measure the elapsed time.
+ *
+ * @param T The time interval in milliseconds.
+ * @param CODE The code block to execute when the timer triggers.
+ *
+ * @return The macro does not return a value.
+ */
+#define TMR8_S(T, CODE)                      \
+  {                                          \
+    static uint8_t tmr##T = 0;               \
+    if ((((uint8_t)millis()) - tmr##T) >= T) \
+    {                                        \
+      tmr##T += T;                           \
+      CODE                                   \
+    }                                        \
+  }
+
+/**
+ * @brief Defines a macro for a timer that triggers after a specified time interval, with a specified code block to execute.
+ * The timer uses the millis() function to measure the elapsed time.
+ *
+ * @param T The time interval in milliseconds.
+ * @param CODE The code block to execute when the timer triggers.
+ *
+ * @return The macro does not return a value.
+ */
+#define TMR16_S(T, CODE)                      \
+  {                                           \
+    static uint16_t tmr##T = 0;               \
+    if ((((uint16_t)millis()) - tmr##T) >= T) \
+    {                                         \
+      tmr##T += T;                            \
+      CODE                                    \
+    }                                         \
+  }
+
+/**
+ * @brief Defines a macro for a timer that triggers after a specified time interval, with a specified code block to execute.
+ * The timer uses the millis() function to measure the elapsed time.
+ *
+ * @param T The time interval in milliseconds.
+ * @param CODE The code block to execute when the timer triggers.
+ *
+ * @return The macro does not return a value.
+ */
+#define TMR32_S(T, CODE)                      \
+  {                                           \
+    static uint32_t tmr##T = 0;               \
+    if ((((uint32_t)millis()) - tmr##T) >= T) \
+    {                                         \
+      tmr##T += T;                            \
+      CODE                                    \
+    }                                         \
+  }
+
+/**
+ * @brief Defines a macro for a timer that triggers after a specified time interval, with a specified code block to execute.
+ * The timer uses the millis() function to measure the elapsed time.
+ *
+ * @param T The time interval in milliseconds.
+ * @param CODE The code block to execute when the timer triggers.
+ *
+ * @return The macro does not return a value.
+ */
+#define TMR64_S(T, CODE)          \
+  {                               \
+    static uint64_t tmr##T = 0;   \
+    if ((millis() - tmr##T) >= T) \
+    {                             \
+      tmr##T += T;                \
+      CODE                        \
+    }                             \
+  }
