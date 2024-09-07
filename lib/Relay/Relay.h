b68bac3519ef7b5934b74c3e299fe7c385986a3c
Bugnutiy <bigbag.minecrafter@gmail.com>
2024-09-07 13:56:56 +0300

    Settings 0

--- lib/Relay/Relay.h
+++ lib/Relay/Relay.h
@@ -1,21 +1,26 @@
 #pragma once
 #include <Arduino.h>
+
 /// @brief Небольшая библиотека для удобного управления модулем переключателя (реле)
 class Relay
 {
 private:
     byte _pin;
     bool _truth, _state, _nextState;
-    uint64_t _min_change_time;
-    void _setState(bool state);
+    uint64_t _min_change_time, _tmr_min_change_time;
+    void _setState(bool state, bool now = 0);
 
 public:
     Relay(byte pin, bool truth = 1, uint64_t min_change_time = 0);
     ~Relay();
     bool set(bool state);
+    bool setNow(bool state);
     bool getState();
     void change();
     void setMinChangeTime(uint64_t t);
+
+    bool ready();
+    void tick();
 };
 
 /// @brief Конструктор класса реле
@@ -44,17 +49,18 @@ bool Relay::set(bool state)
 {
     if (getState() != state)
     {
-        if (!_min_change_time)
-        {
-            _setState(state);
-            digitalWrite(_pin, _state);
-            return true;
-        }
-        else
-        {
+        _setState(state);
+        return true;
+    }
+    return false;
+}
 
-
-        }
+bool Relay::setNow(bool state)
+{
+    if (getState() != state)
+    {
+        _setState(state, 1);
+        return true;
     }
     return false;
 }
@@ -69,17 +75,71 @@ bool Relay::getState()
 void Relay::change()
 {
     _setState(!getState());
-    digitalWrite(_pin, _state);
 }
 
-/// @brief 
-/// @param t 
-void Relay::setMinChangeTime(uint64_t t)
+/**
+ * @brief Sets the minimum change time for the relay.
+ *
+ * This function sets the minimum time interval between two consecutive changes of the relay's state.
+ * If the time elapsed since the last change is less than the minimum change time, the relay will not be changed.
+ *
+ * @param min_change_time The minimum change time in milliseconds.
+ */
+void Relay::setMinChangeTime(uint64_t min_change_time)
 {
-    _min_change_time = t;
+    _min_change_time = min_change_time;
 }
 
-void Relay::_setState(bool state)
+/**
+ * @brief Checks if the relay is ready to be changed.
+ *
+ * This function checks if the relay is ready to be changed based on the minimum change time.
+ * If the minimum change time is not set, the function returns true, indicating that the relay is ready.
+ * If the minimum change time is set, the function checks if the time elapsed since the last change is greater than or equal to the minimum change time.
+ * If the time elapsed is greater than or equal to the minimum change time, the function returns true, indicating that the relay is ready.
+ * Otherwise, the function returns false, indicating that the relay is not ready.
+ *
+ * @return true if the relay is ready to be changed, false otherwise.
+ */
+bool Relay::ready()
 {
-    _state = state ^ !_truth;
+    if (!_min_change_time)
+        return true;
+    if (millis() - _tmr_min_change_time >= _min_change_time)
+    {
+        return true;
+    }
+    return false;
+}
+
+void Relay::tick()
+{
+    if (ready())
+    {
+        if (_state != _nextState)
+        {
+            _state = _nextState;
+            _tmr_min_change_time = millis();
+            digitalWrite(_pin, _state);
+        }
+    }
+}
+
+void Relay::_setState(bool state, bool now)
+{
+    if (!_min_change_time)
+    {
+        _state = state ^ !_truth;
+        digitalWrite(_pin, _state);
+    }
+    if (ready() || now)
+    {
+        _state = state ^ !_truth;
+        _tmr_min_change_time = millis();
+        digitalWrite(_pin, _state);
+    }
+    else
+    {
+        _nextState = state ^ !_truth;
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
-
+#include "Timer.h"
 #include "GyverDS18Single.h"
 #include "Relay.h"
+#include <EEManager.h>
 #define MY_DEBUG
 #include "My_Debug.h"
 
@@ -13,23 +14,45 @@
 #define PIN_LED_FRIDGE 10
 #define PIN_LED_FREEZER 11
 
-#define NO_FROST_ON_TIMER 1000 * 60 * 60 * 24 * 7 // 604 800 000 неделя
-#define NO_FROST_OFF_TIMER 1000 * 60 * 60 * 2     // 2 часа 3600000
-#define NO_FROST_EEPROM_TIMER 1000 * 60 * 60 * 24 // День
+#define PIN_POT_L 5
+#define PIN_POT_R 6
+
+#define NO_FROST_ON_TIMER (1000 * 60 * 60 * 24 * 7) // 604 800 000 неделя
+#define NO_FROST_OFF_TIMER (1000 * 60 * 60 * 2)     // 2 часа
+#define NO_FROST_EEPROM_TIMER (1000 * 60 * 60 * 24) // как часто писать в EEPROM время работы без NO_FROST
 #define NO_FROST_MAX_FRIDGE_TEMPERATURE 10
-#define NO_FROST_MAX_FREEZER_TEMPERATURE -5
+#define NO_FROST_MAX_FREEZER_TEMPERATURE -10
 
-#define MAX_WORK_TIME 1000 * 60 * 60 * 3 // 3 часа работает
-#define BREAK_TIMER 1000 * 60 * 10       // Перерыв
-#define BREAK_RESET 1000 * 60 * 5        // Сброс таймера работы
+#define MAX_WORK_TIME (1000 * 60 * 60 * 3) // 3 часа работает
+#define BREAK_TIMER (1000 * 60 * 10)       // Перерыв
+#define BREAK_RESET (1000 * 60 * 5)        // Сброс таймера работы
 
-#define RELAY_SET_TIMER 1000 * 60 * 5 // Как часто можно переключать реле
+#define RELAY_CHANGE_TIMER (1000 * 60 * 5) // Как часто можно переключать реле
 
-GyverDS18Single sensorFridge();
-GyverDS18Single sensorFrizer();
+#define TEMP_FREEZER_MIN -30
+#define TEMP_FREEZER_MAX -10
+#define TEMP_FREEZER_DEFAULT_RANGE 5
+#define TEMP_FREEZER_MAX_RANGE 10
+#define TEMP_FREEZER_MIN_RANGE 1
 
+#define TEMP_FRIDGE_MIN -10
+#define TEMP_FRIDGE_MAX 10
+#define TEMP_FRIDGE_DEFAULT_RANGE 5
+#define TEMP_FRIDGE_MAX_RANGE 10
+#define TEMP_FRIDGE_MIN_RANGE 1
+
+GyverDS18Single sensorFridge(PIN_SENS_FRIDGE, 0);
+GyverDS18Single sensorFrizer(PIN_SENS_FREEZER, 0);
+
+Relay compressor(PIN_RELAY, 1, RELAY_CHANGE_TIMER);
+
+uint64_t noFrostWorkTime = 0;
+EEManager memory(noFrostWorkTime);
 void setup()
 {
+#ifdef MY_DEBUG
+    Serial.begin(115200);
+#endif
 }
 
 void loop()
